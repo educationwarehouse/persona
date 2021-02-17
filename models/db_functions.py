725de4594d1091da96994cf07abe6e6b29ba202a
kevinkosterr <koster.k2001@gmail.com>
2021-02-17 17:17:47 +0100

    * clarified some of the form.errors * renamed NO_MEMBERSHIP_OVERLAP to NO_MEMBERSHIP_PERIOD_OVERLAP * NO_MEMBERSHIP_PERIOD_OVERLAP now shows the user all errors at once, this way a user doesn't have to go through failure more than once. (not guaranteed) * changed some of the documentation to match the current logic * renamed check_conflicts() to is_date_within_active_membership_record(), this has also been simplified. - removed the need for an exclusion in is_date_within_active_membership_record()

--- models/db_functions.py
+++ models/db_functions.py
@@ -1,9 +1,12 @@
 import datetime
 
 
-def NO_MEMBERSHIP_OVERLAP(form):
-    """Validator to check whether or not the selected role is overlapping with a role that is already assigned to a
-    dossier in the given period of time.
+def NO_MEMBERSHIP_PERIOD_OVERLAP(form):
+    """Validator to check whether or not a new role_membership period is overlapping with the active role_membership
+    period assigned to a person.
+
+    Meaning, if the begin date of a new role_membership is within the range of the begin- and end date of the active
+    role_membership, an error message will be shown.
 
     Example:
         Usage:
@@ -14,61 +17,77 @@ def NO_MEMBERSHIP_OVERLAP(form):
     """
     if not form.vars.role_ids:
         form.errors.role_ids = T('Select a role to assign to this role membership')
-        return
+
     # form.vars.person_id doesn't work here. as the person_id doesn't get submitted with the form vars.
+    active_role_membership = active_role_membership_record(request.vars['person'])
+
+    # if the person does not have any active role memberships, accept validation.
+    if not active_role_membership:
+        return True
+
+    # form.record indicates that a record is being edited.
     if form.record:
-        role_memberships = active_role_memberships(form.record.person_id)
-    else:
-        role_memberships = active_role_memberships(request.vars['person'])
+        # if the record we're trying to edit is the same as the active role_membership record, accept validation.
+        if form.record.id == active_role_membership.id:
+            return True
 
     begin_date = form.vars.begin_date
-    current_role_membership = form.record.id if form.record else []
-    exclude = None
+    end_date = form.vars.end_date
 
-    if current_role_membership:
-        # selecting the record, because check_conflicts() creates a list of conflicting records
-        # we use this to remove the current record from that list.
-        exclude = db(db.role_membership.id == current_role_membership).select().first()
+    if begin_date is not None and end_date is not None:
+        #  a membership can't end sooner than it begins, so if begin_date > end_date, show error message.
+        if begin_date > end_date:
+            form.errors.end_date = T("Membership can't end sooner than it begins. "
+                                     "Please change the end date to a later date")
 
-    conflicting = check_conflicts(active_role_memberships=role_memberships,
-                                  begin_date=begin_date,
-                                  exclude=exclude)
-    if conflicting:
-        form.errors.begin_date = T('This membership overlaps with another membership')
+    conflict = is_date_within_active_membership_record(active_role_membership_record=active_role_membership,
+                                                       begin_date=begin_date)
+
+    if conflict:
+        form.errors.end_date = T('New membership period overlaps with the currently active membership period. If '
+                                 'you want to split the active period, change the end date of the active membership '
+                                 'and start a new period with the new set of roles.')
+
+    # if form has errors, show all of the errors at once.
+    if form.errors:
         return
 
 
-def active_role_memberships(person_id):
-    """Gets the currently active role_membership records assigned to a person.
+def active_role_membership_record(person_id):
+    """Gets the currently active role_membership record assigned to a person.
 
-    :param person_id: id of the person you want the role_membership records of
-    :return: list of role_membership records
+    :param person_id: id of the person you want the active role_membership record of
+    :return: active role_membership record
     """
     today = datetime.date.today()
     query = (db.role_membership.person_id == person_id) & (db.role_membership.begin_date <= today)
     query &= (db.role_membership.end_date >= today) | (db.role_membership.end_date == None)
     rows = db(query).select()
-    # creating list of all rows given by the select statement
-    memberships = [r for r in rows]
-    return memberships
+    # raise a ValueError if there is more than one row, since we only allow one role_membership to be active at a time.
+    if len(rows) > 1:
+        raise ValueError(T('There can only be 1 active role_membership record at a time.'))
+    return rows.first()
 
 
-def check_conflicts(active_role_memberships, exclude, begin_date=None):
-    """Checks if there are any memberships that conflict with the current membership we're trying to insert.
+def is_date_within_active_membership_record(active_role_membership_record, begin_date=None):
+    """Checks if the given date is in the role_membership that's currently active.
 
-    :param active_role_memberships: the role_membership records you need to check, use active_role_memberships() for this.
-    :param exclude: the record that's allowed to be overridden
+    :param active_role_membership_record: an active role_membership record, use active_role_membership_record()
     :param begin_date: the starting date of the new role_membership record.
-    :return: either True or False
+    :return: boolean
     """
     if begin_date:
-        # creating a list of conflicting records. we do not allow any role membership to overlap
-        conflicts = [r for r in active_role_memberships if r.begin_date <= begin_date <= r.end_date if
-                     not r.end_date is None or not r.begin_date is None]
-        # override is the current record we're trying to edit, so it will be removed from the list of conflicts.
-        if exclude:
-            conflicts.remove(exclude) if exclude in conflicts else []
-        return bool(conflicts)
+
+        if active_role_membership_record.end_date:
+            return bool(
+                active_role_membership_record.begin_date <= begin_date <= active_role_membership_record.end_date)
+
+        else:
+            # we do not allow different role memberships to start on the same day, even when they have no ending
+            # date. if we were to allow the same begin date on different records, we would at one point get multiple
+            # active role memberships.
+            return bool(active_role_membership_record.begin_date == begin_date)
+
     # obviously, if no begin date has been given, this membership can't overlap
     else:
         return False
